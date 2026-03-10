396394a6ba500493c079b59ac544c6bbfc09b63b
nabilibnualrasyid <nabilibnualrasyid37@gmail.com>
2026-03-10 14:41:03 +0700

    P2

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <h1>Halaman Kontak</h1>
+    <p>Silakan menghubungi kami jika ada pertanyaan terkait event atau pemesanan tiket.</p>
+    <p>Email: support@eventticket.com</p>
+    <p>WhatsApp: 081234567890</p>
+</div>
