855231b9c2681cb640bceecb606ee0b209e14a63
nabilibnualrasyid <nabilibnualrasyid37@gmail.com>
2026-04-06 12:15:58 +0700

    Revert "p3"
    This reverts commit 40492de9a54132efd7cc55d027a03754a77775c8.

--- routes/web.php
+++ routes/web.php
@@ -6,23 +6,3 @@
  
 Route::get('/', [HomeController::class, 'index']); 
 Route::get('/contact', [HomeController::class, 'contact']); 
-
-Route::get('/welcome', function () {
-    return view('welcome');
-});
-
-Route::get('/user/{id}', function ($id) {
-    return 'user dengan ID ' . $id;
-});
-
-Route::prefix('admin')->group(function () {
-
-    Route::get('/dashboard', function () {
-        return 'Admin Dashboard';
-    });
-
-    Route::get('/users', function () {
-        return 'Admin Users';
-    });
-
-});
