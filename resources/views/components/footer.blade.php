d589fa27ac9988ef289aa6fbb8a1f945df47c6be
nabilibnualrasyid <nabilibnualrasyid37@gmail.com>
2026-05-03 22:33:15 +0700

    p6

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-white dark:bg-gray-900 border-t mt-10">
+    <div class="max-w-screen-xl mx-auto p-6 text-center text-gray-500">
+        <p>&copy; {{ date('Y') }} MyApp. All rights reserved.</p>
+    </div>
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ListBarangController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\EventController;
-use App\Http\Controllers\BarangController;
 use App\Http\Controllers\TicketController;
+use App\Http\Controllers\ProductController;
 
+Route::get('/', function () {
+    return redirect('/pages/home');
+});
 
-Route::view('/', 'home');
-Route::view('/home', 'home');
-Route::view('/about', 'about');
-Route::view('/product', 'product'); 
-Route::view('/contact', 'contact');
-Route::view('/login', 'login');
-Route::view('/register', 'register');
-Route::view('/dashboard', 'dashboard');
+// Halaman utama dengan prefix pages
+Route::prefix('pages')->group(function () {
+    Route::view('/home', 'pages.home');
+    Route::view('/about', 'pages.about');
+    Route::view('/product', 'pages.product');
+    Route::view('/contact', 'pages.contact');
+    Route::view('/dashboard', 'pages.dashboard');
+    Route::view('/event', 'pages.event');
+    Route::view('/list-ticket', 'pages.list_ticket');
+    Route::view('/welcome', 'pages.welcome');
+});
+Route::view('/login', 'pages.login');
+Route::view('/register', 'pages.register');
 Route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
 Route::get('/ticket-list', [TicketController::class, 'tampilkan']);
+Route::get('/products', [ProductController::class, 'index']);
